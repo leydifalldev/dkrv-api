b1ea2f64eea5e4072c4b50eca24ffda8a662ccdc
leydifalldev <doudou.leydi.fall@gmail.com>
2019-10-16 01:04:47 +0200

    dkrv-bo wip

--- dkrv-bo/src/screens/Place/retrieve/detail.js
+++ dkrv-bo/src/screens/Place/retrieve/detail.js
@@ -1,50 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {useQuery} from '@apollo/react-hooks'
-import { RETRIEVE_PLACE_DETAIL } from '../../../network/index';
-import { SimpleCard } from '../../../components/Card';
-import { LikesStatCard, TotalVisitCard } from '../../components/Card';
+import { useQuery } from "@apollo/react-hooks";
+import { Tabs, Tab } from "react-materialize";
+import { RETRIEVE_PLACE_DETAIL } from "../../../network/index";
+import { SimpleCard } from "../../../components/Card";
+import { LikesStatCard, TotalVisitCard } from "../../components/Card";
 
 export const PlaceDetail = () => {
   let { id } = useParams();
-  const { loading, error, data } = useQuery(RETRIEVE_PLACE_DETAIL, {variables: {id}});
-  console.log(data);
+
+  const { loading, error, place } = useQuery(RETRIEVE_PLACE_DETAIL, {
+    variables: { id }
+  });
+
   return (
     <div className="row">
       <div className="col s3">
-        <SimpleCard />
+        <SimpleCard label={"name"} />
       </div>
       <div className="col s9">
-        <StatPanel/>
-        <PlaceInfoCollection/>
+        <StatPanel />
+        <PlaceInfoTabs />
       </div>
     </div>
   );
-}
+};
+
+const PlaceInfoTabs = () => (
+  <Tabs className="tabs" options={{ swipeable: true }}>
+    <Tab title="Informations générales" active className="">
+      <PlaceInfoCollection />
+    </Tab>
+    <Tab title="Horaires" className="red">
+      Test 2
+    </Tab>
+    <Tab title="Photos" className="red">
+      Test 2
+    </Tab>
+    <Tab title="Évènements" className="green">
+      Test 3
+    </Tab>
+  </Tabs>
+);
 
 const PlaceInfoCollection = () => (
   <div>
     <ul class="collection with-header">
-        <li class="collection-header"><h4>First Names</h4></li>
-        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-        <li class="collection-item"><div>Alvin<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-      </ul>
+      <li class="collection-header">
+        <h4>First Names</h4>
+      </li>
+      <li class="collection-item">
+        <div>
+          Alvin
+          <a href="#!" class="secondary-content">
+            <i class="material-icons">send</i>
+          </a>
+        </div>
+      </li>
+      <li class="collection-item">
+        <div>
+          Alvin
+          <a href="#!" class="secondary-content">
+            <i class="material-icons">send</i>
+          </a>
+        </div>
+      </li>
+      <li class="collection-item">
+        <div>
+          Alvin
+          <a href="#!" class="secondary-content">
+            <i class="material-icons">send</i>
+          </a>
+        </div>
+      </li>
+      <li class="collection-item">
+        <div>
+          Alvin
+          <a href="#!" class="secondary-content">
+            <i class="material-icons">send</i>
+          </a>
+        </div>
+      </li>
+    </ul>
   </div>
-)
+);
 
 const StatPanel = () => (
   <div className="row">
     <div className="col s4">
-      <TotalVisitCard/>
+      <TotalVisitCard />
     </div>
     <div className="col s4">
-      <LikesStatCard/>
+      <LikesStatCard />
     </div>
-    <div className="col s4">
-      
-    </div>
+    <div className="col s4"></div>
   </div>
-)
-
+);
--- dkrv-bo/src/screens/Place/retrieve/list.js
+++ dkrv-bo/src/screens/Place/retrieve/list.js
@@ -1,49 +1,46 @@
-import React, {Fragment} from 'react';
-import {useQuery} from '@apollo/react-hooks'
-import { SimpleCard } from '../../../components/Card';
-import { RETRIEVE_PLACE_LIST } from '../../../network';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { SimpleCard } from "../../../components/Card";
+import { RETRIEVE_PLACE_LIST } from "../../../network";
 import { Link } from "react-router-dom";
 
 export const PlaceList = () => {
   const { loading, error, data } = useQuery(RETRIEVE_PLACE_LIST);
   console.log(data);
-  return data && data.places.length ? (<ListCard style={placeContainerStyle} items={data.places}/>) : <ErrorPanel/>
-}
+  return data && data.places.length ? (
+    <ListCard style={placeContainerStyle} items={data.places} />
+  ) : (
+    <ErrorPanel />
+  );
+};
 
 const ErrorPanel = () => (
-  <div class="row">
-    <div class="col s12 m5">
-      <div class="card-panel teal">
-        <span class="white-text">
-          La liste est vide
-        </span>
-      </div>
-    </div>
+  <div class="card-panel teal">
+    <span class="white-text">La liste est vide</span>
   </div>
 );
 
-const ListCard = ({items}) => (
+const ListCard = ({ items }) => (
   <div className="row">
-    { items.map(item => 
+    {items.map(item => (
       <div className="col s2">
-        <Link to={{ pathname: '/place/'+item.id}}
-              key={item.id}>
-          <SimpleCard style={cardStyle} key={item.id} item={item}/>
+        <Link to={{ pathname: "/place/" + item.id }} key={item.id}>
+          <SimpleCard style={cardStyle} key={item.id} item={item} />
         </Link>
-      </div>)
-    }
+      </div>
+    ))}
   </div>
-)
+);
 
 const placeContainerStyle = {
-  backgroundColor: '#f8f9fa !important',
-}
+  backgroundColor: "#f8f9fa !important"
+};
 
 const cardStyle = {
-  border: '1px solid rgba(0,0,0,.125)',
-  transition: 'none',
-  backgroundColor: '#FFFFFF',
-  boxShadow: '0 .125rem .25rem rgba(0,0,0,.075)',
-  backgroundClip: 'border-box',
-  borderRadius: '.25em'
+  border: "1px solid rgba(0,0,0,.125)",
+  transition: "none",
+  backgroundColor: "#FFFFFF",
+  boxShadow: "0 .125rem .25rem rgba(0,0,0,.075)",
+  backgroundClip: "border-box",
+  borderRadius: ".25em"
 };
